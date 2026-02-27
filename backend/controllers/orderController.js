b3e992c2128fea9b4da9bba3159f7747e4b978a1
guhan-21 <guhanmuruganantham21@gmail.com>
2026-02-27 12:15:11 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -10,7 +10,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // Placing user order for frontend
 const placeOrder = async (req, res) => {
 
-        const frontend_url = "http://localhost:5174";
+        const frontend_url = "https://food-dell-frontend-b84g.onrender.com/cart";
 
 
     console.log(req.body.Items);
